0f395af4ffd6f0ef7e915398d0ab9f41e899658c
Raidy Arenas <raidy.arenas@gmail.com>
2021-02-03 15:33:44 -0400

    [ADD] added view register

--- src/app/modules/no-auth/components/register/register.component.ts
+++ src/app/modules/no-auth/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Helpers, PATTERNS} from "../../../../helpers/Helpers";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formRegister: FormGroup;
+  getMessage = Helpers.getMessageError;
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit(): void {
+    this.initFormRegister();
+  }
+
+  initFormRegister(): void {
+    this.formRegister = this.fb.group({
+      first_name: ['', [Validators.required, Validators.pattern(PATTERNS.only_characters)]],
+      last_name: ['', [Validators.required, Validators.pattern(PATTERNS.only_characters)]],
+      birthday: ['', [Validators.required, Validators.pattern(PATTERNS.date)]],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  submit(): void {
+    alert("[Register User]")
   }
 
 }
+
+
